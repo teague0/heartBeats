15e6325a7a571987f6cb56fc8b6049e95145389d
James Lee <james.lee-5@selu.edu>
2021-07-12 14:37:59 -0500

    Location Table

--- script/Location Table .R
+++ script/Location Table .R
@@ -6,12 +6,16 @@ B.locs <- which(beats$abBehav == "B")
 D.locs <- which(beats$abBehav == "D")
 C.locs <- which(beats$abBehav == "C")
 
+#Creating data frames for A and E. Naming BC points as bottom.point. Named D as 
+#up.point. Made a  data fram with B and D locs 
 locDat <- data.frame(A.locs, E.locs) 
 bDat <- data.frame(bottom.point = B.locs, type = 'B')
 dDat <- data.frame(up.point = D.locs, type = 'D')
 bdDaT <- data.frame(B.locs, D.locs)
 cDat <- data.frame(bottom.point = C.locs, type = "C")
 
+#Bound Bdat and Cdat. Bound dDat to cDat. Bound low to up and arranged it from 
+# the first bottom point. Got rid of "uneeded" columns. Renamed the columns. Made a dataframe
 low <- bind_rows(bDat, cDat)
 up <- bind_rows(dDat, cDat)
 lowup <- bind_cols(low, up)
@@ -21,9 +25,11 @@ lowup$type...4 <- NULL
 names(lowup) <- c("BC.locs", "BCType", "D.locs")
 locDat <- data.frame(A.locs, lowup, E.locs)
 
+#made a period column
 nums <- seq(1:length(locDat$A.locs))
 locDat$periodNum <- paste0("p.", nums)
 
+#graph
 tmp <- beats[(locDat$A.locs[1]-500):(locDat$E.locs[1]+500),]
 ggplot(tmp)+
   geom_line(aes(x = timestamp, y = heartRate.bpm))
