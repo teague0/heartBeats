27101960146bf45275741ec09a0f4f807fc73e3f
unknown <matthew.giblin@selu.edu>
2021-07-13 13:45:41 -0500

    upload git

--- script/Matt R Files.R
+++ script/Matt R Files.R
@@ -12,38 +12,38 @@ locDat %>%
 map_dbl(~ max(.x)) %>%
   map2(~ min(A.locs))
   range()
- 
-  #repost
-  A.locs <- which(beats$abBehav == "A")
-  E.locs <- which(beats$abBehav == "E")
-  B.locs <- which(beats$abBehav == "B")
-  D.locs <- which(beats$abBehav == "D")
-  C.locs <- which(beats$abBehav == "C")
-  locDat <- data.frame(A.locs, E.locs) 
-  bDat <- data.frame(bottom.point = B.locs, type = 'B')
-  dDat <- data.frame(type = 'D')
-  bdDaT <- data.frame(B.locs, D.locs)
-  cDat <- data.frame(bottom.point = C.locs, type = "C")
 
   
   #trying "for" to find a way to make D column have C values
-  for (BCType == "C") 
+  for(BCType == "C") 
     print(string, D.locs)
   } else if (D.locs = NA) +
     seek(value -> BCType)
-    }
+
+map(locDat$D.locs, ifelse(is.na(), locDat$C.locs)
+    
+for(i in 1:length(locDat$D.locs)){
+  locDat$D.locs[i] = ifelse(is.na(locDat$D.locs[i]), locDat$C.locs[i], locaDat$D.locs[i])
+}
+    
+
+locDat
 rows.in.BC.locs.that.are.not.in.D.locs(BC.locs, D.locs)
 within(locDat, D.locs[is.NA(n)] <- BC.locs[is.value(n)])
 
-df <- sample(1:100, 33 * 332, replace = TRUE) %>%
-  matrix(ncol = 33) %>%
-  as.data.frame()
-y <- map(3:27,
-         function(i) {
-           map(1:272,
-               function(j, i) {
-                 map_dbl(28:33,
-                         function(k, j, i){lm(df[j:(60+j), i] ~ df[j:(60+j), k])[[1]][2]}, 
-                         j = j, i = i)
-               }, i = i)
-         })
+#setting up calculations for percentage change between D and E 
+#used this code for setting heart rate to points in A and B locations
+> locDat$A.hr <- beats$heartRate.bpm[locDat$A.locs]
+> locDat$B.hr <- beats$heartRate.bpm[locDat$BC.locs]
+pDrop.AB <- (locDat$A.hr - locDat$B.hr) / locDat$A.hr*100
+locDat$pDrop.AB <- (locDat$A.hr - locDat$B.hr) / locDat$A.hr*100
+
+growth_rate <- ()
+for(i in 1:length(locDat$D.locs))
+
+locDat %>%
+  select(A.hr, B.hr) %>%
+  rate percent = 
+for((i in 1:)(A.hr, B.hr) *100)
+
+
