3e7d94efb8b0479515417dd8974fd61fd4ef4f32
James Lee <james.lee-5@selu.edu>
2021-07-13 18:59:38 -0500

    Location Table with beginning of regression model attempts

--- script/Location Table .R
+++ script/Location Table .R
@@ -20,16 +20,34 @@ low <- bind_rows(bDat, cDat)
 up <- bind_rows(dDat, cDat)
 lowup <- bind_cols(low, up)
 lowup <- lowup %>% arrange(bottom.point...1)
-lowup$bottom.point...5 <- NULL
-lowup$type...4 <- NULL
-names(lowup) <- c("BC.locs", "BCType", "D.locs")
+
+names(lowup) <- c("BC.locs", "BCType", "D.locs", "DCType", "C.locs")
 locDat <- data.frame(A.locs, lowup, E.locs)
 
 #made a period column
 nums <- seq(1:length(locDat$A.locs))
 locDat$periodNum <- paste0("p.", nums)
 
+#Put the C&D locations together
+locDat$DC.locs <- locDat$D.locs
+Ccopy <- which(!is.na(locDat$C.locs))
+locDat$DC.locs[Ccopy] <- locDat$C.locs[Ccopy]
+
 #graph
 tmp <- beats[(locDat$A.locs[1]-500):(locDat$E.locs[1]+500),]
 ggplot(tmp)+
   geom_line(aes(x = timestamp, y = heartRate.bpm))
+
+str(mtcars)
+m1 <- lm(mpg~disp, data = mtcars)
+
+
+#rearrange the columns
+col_order <- c("A.locs", "BC.locs", "BCType", "DC.locs", "DCType", "E.locs", "D.locs", "C.locs")
+locDat <- locDat[, col_order]
+
+ABreg <- lm(A.locs~BC.locs, data= locDat)
+RegAB <- lm(ABheart~as.numeric(ABTime))
+
+ABheart <- beats$heartRate.bpm[locDat$A.locs[1]:locDat$BC.locs[1]]
+ABTime <- beats$timestamp[locDat$A.locs[1]:locDat$BC.locs[1]]
