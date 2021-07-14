1d0af10be9b64f885a157e19c4c7a64ab5564e84
unknown <matthew.giblin@selu.edu>
2021-07-14 14:56:15 -0500

    new AE, BD values

--- script/BD Duration.R
+++ script/BD Duration.R
@@ -2,9 +2,28 @@
 
 locDat$BDdur.s <- NA
 for(i in 1:length(locDat$A.locs)){
-  if(locDat$BCType == "C"){next}
+  if(locDat$BCType[i] == "C"){next} #for C
   t1 <- beats$timestamp[locDat$BC.locs[i]]
   t2 <- beats$timestamp[locDat$DC.locs[i]]
   locDat$BDdur.s[i] <- as.numeric(t2) - as.numeric(t1)
 }
+for(i in 1:length(locDat$A.locs)){
+  t3 <- beats$timestamp[locDat$A.locs[i]]
+  t4 <- beats$timestamp[locDat$E.locs[i]]
+  locDat$AEdur.s[i] <- as.numeric(t4) - as.numeric(t3)
+}
+locDat %>%
+select("BCType") %>%
+  geom_point(mapping = aes(x = BCType, y = locDat$AEdur.s [i], value C = blue, = green )
+             
+ggplot(locDat)+
+  geom_boxplot(aes(x = BCType, y = AEdur.s, fill = BCType))
+            
 
+beats %>%
+  filter(batID == "inga", dayNight == "day") %>%
+  select(batID, dayNight, heartRate.bpm)
+  t5 <- beats$timestamp[locDat$E.locs [i]]
+  t6 <- beats$timestamp[locDat$A.locs [i + 1]]
+  within.data.frame([i +1] = locDat$A.locs)
+  locDat$EAdur.s[i] <- as.numeric(t5) - as.numeric(t6)
--- script/Matt R Files.R
+++ script/Matt R Files.R
@@ -1,11 +1,13 @@
+library(tidyverse)
+library(lubridate)
+library(plotly)
+
 #possibly using Map() function
-beats %>%
+ %>%
   map(abBehav)
-filter(abBehav == "A", abBehav == "B", abBehav == "C", AbBehav == "D", abBehav == "E") %>%
+filter() %>%
 select(abBehav) %>%
 
-  
-  
   # finding the maximum and minimum values that the start "A" of the drop can fluctuate
 locDat %>%
   select(A.locs) %>%
@@ -15,35 +17,42 @@ map_dbl(~ max(.x)) %>%
 
   
   #trying "for" to find a way to make D column have C values
-  for(BCType == "C") 
-    print(string, D.locs)
-  } else if (D.locs = NA) +
-    seek(value -> BCType)
-
 map(locDat$D.locs, ifelse(is.na(), locDat$C.locs)
     
 for(i in 1:length(locDat$D.locs)){
   locDat$D.locs[i] = ifelse(is.na(locDat$D.locs[i]), locDat$C.locs[i], locaDat$D.locs[i])
 }
-    
 
-locDat
-rows.in.BC.locs.that.are.not.in.D.locs(BC.locs, D.locs)
-within(locDat, D.locs[is.NA(n)] <- BC.locs[is.value(n)])
-
-#setting up calculations for percentage change between D and E 
+#setting up calculations for 100 percentage change between D and E 
 #used this code for setting heart rate to points in A and B locations
-> locDat$A.hr <- beats$heartRate.bpm[locDat$A.locs]
-> locDat$B.hr <- beats$heartRate.bpm[locDat$BC.locs]
+locDat$A.hr <- beats$heartRate.bpm[locDat$A.locs]
+locDat$B.hr <- beats$heartRate.bpm[locDat$BC.locs]
+locDat$D.hr <- beats$heartRate.bpm[locDat$DC.locs]
+locDat$E.hr <- beats$heartRate.bpm[locDat$E.locs]
 pDrop.AB <- (locDat$A.hr - locDat$B.hr) / locDat$A.hr*100
 locDat$pDrop.AB <- (locDat$A.hr - locDat$B.hr) / locDat$A.hr*100
-
-growth_rate <- ()
-for(i in 1:length(locDat$D.locs))
+pRise.DE <- (locDat$E.hr - locDat$D.hr) / locDat$E.hr*100
+locDat$pRiseDE <- (locDat$E.hr - locDat$D.hr) / locDat$E.hr*100
 
 locDat %>%
   select(A.hr, B.hr) %>%
   rate percent = 
 for((i in 1:)(A.hr, B.hr) *100)
+  
+#distance between A and E. First accounting for presence of C
+  locDat$BDdur.s <- NA
+for(i in 1:length(locDat$A.locs)){
+  if(locDat$BCType[i] == "C"){next} #for C
+  t1 <- beats$timestamp[locDat$BC.locs[i]]
+  t2 <- beats$timestamp[locDat$DC.locs[i]]
+  locDat$BDdur.s[i] <- as.numeric(t2) - as.numeric(t1)
+}
+for(i in 1:length(locDat$A.locs)){
+  t3 <- beats$timestamp[locDat$A.locs[i]]
+  t4 <- beats$timestamp[locDat$E.locs[i]]
+  # need code to disregard first value of A and last value of E
+  locDat$AEdur.s[i] <- as.numeric(t4) - as.numeric(t3)
+}
+#distance between E and the next A
 
 
